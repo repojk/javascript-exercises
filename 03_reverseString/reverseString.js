710bdce5a4b1371def52688d2af118817388b0e1
repojk <gitofjk@gmail.com>
2022-09-14 11:28:01 -0700

    Implement reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    // convert to array to reverse
+    let result = word.split("");
+    result = result.reverse();
+    return result.join("");
 };
 
 // Do not edit below this line
