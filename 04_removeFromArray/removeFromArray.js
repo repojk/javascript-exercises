11be2845878a54c14d3f909c581127dcf860be3e
repojk <gitofjk@gmail.com>
2022-09-14 13:32:22 -0700

    Implement removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arrayInitial, ...numsRemove) {
+    let result = arguments[0];
+    for (let i = 0; i < numsRemove.length; i++) {
+        for (let j = 0; j < result.length; j++) {
+            if (numsRemove[i] === result[j]) {
+                result.splice(j,1);
+            }
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
