8118e71ec1bf15cf366df16c5122c5c21c396f93
repojk <gitofjk@gmail.com>
2022-09-14 14:13:53 -0700

    Implement sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+    for (let arg of args) {
+        if (typeof arg !== "number") {
+            return "ERROR";
+        }
+        if (arg < 0) {
+            return "ERROR";
+        }
+    }
 
+    let argSmall = 0;
+    let argLarge = 0;
+    if (args[0] < args[1]) {
+        argSmall = args[0];
+        argLarge = args[1];
+    }
+    else {
+        argSmall = args[1];
+        argLarge = args[0];
+    }
+    
+    let result = 0;
+    for (let i = argSmall; i < (argLarge + 1); i++) {
+        result += i;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
