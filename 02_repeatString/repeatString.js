b0a56f4a6b310648a9c2ca5f282779665d0dae56
repojk <gitofjk@gmail.com>
2022-09-14 11:19:24 -0700

    Implement repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(stringInput, numRepeat) {
+    if (numRepeat < 0) {
+        return "ERROR"
+    }
+    
+    let result = ""
+    for (let i = 0; i < numRepeat; i++) {
+        result += stringInput;
+    }
+    return result;
 };
 
 // Do not edit below this line
